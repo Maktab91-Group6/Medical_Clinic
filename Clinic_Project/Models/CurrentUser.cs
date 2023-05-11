4f2d7e48d57d07bb4b3012d8cccd40bcbd065a90
EM80AFR <emadafrasiyabi14@gmail.com>
2023-05-11 10:32:48 +0330

    Add Current User to project

---
+++ Clinic_Project/Models/CurrentUser.cs
@@ -0,0 +1,17 @@
+﻿namespace Clinic_Project.Models
+{
+    public static class CurrentUser
+    {
+        public static int CurrentUserId { get; set; }
+        public static void SetCurrentUser(int id)
+        {
+            CurrentUserId = id;
+        }
+        public static void RemoveCurrentUser()
+        {
+            CurrentUserId = default;
+        }
+    }
+
+    
+}
